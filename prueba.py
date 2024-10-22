84c31d3fdb7d43eb1793b582a3797af50a138ecb
eduqui <eduandresq67@gmail.com>
2024-10-22 17:29:38 -0300

    probando 2

--- prueba.py
+++ prueba.py
@@ -1,13 +1,2 @@
-# Tabla de multiplicar de un número
-numero = 7  
-for i in range(1, 11):
-    print(f"{numero} x {i} = {numero * i}")
-
-numero = 5  
-for i in range(1, 11):
-    print(f"{numero} x {i} = {numero * i}")
-
-
-print('prueba de que sabemos algo')
-
-print("Nooo sabemos nada")
+print('Realizando pruebas')
+print('prueba 2')
--- prueba2.py
+++
@@ -1,9 +0,0 @@
-numero = 5  
-for i in range(1, 11):
-    print(f"{numero} x {i} = {numero * i}")
-
-print("Hola soy Clari")
-
-print("Realizando otra prueba")
-
-print("Otra prueba más de Clari :)")
